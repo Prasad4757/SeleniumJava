d0dbabec504959664c9428ec065bf27a27f085c8
Prasad4757 <Prasad.Hanumant.Band@gds.ey.com>
2026-02-26 17:01:48 +0530

    Update basic.java

--- src/test/java/org/example/basic.java
+++ src/test/java/org/example/basic.java
@@ -2,6 +2,7 @@ package org.example;
 
 import junit.framework.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -17,16 +18,34 @@ public class basic {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-        WebElement drpdown= driver.findElement(By.xpath("//select[@id=\"dropdown-class-example\"]"));
-        Select s=new Select(drpdown);
-        s.selectByValue("option2");
-        WebElement selectedoption=s.getFirstSelectedOption();
-        System.out.println(selectedoption.getText());
+//        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+//        WebElement drpdown= driver.findElement(By.xpath("//select[@id=\"dropdown-class-example\"]"));
+//        Select s=new Select(drpdown);
+//        s.selectByValue("option2");
+//        WebElement selectedoption=s.getFirstSelectedOption();
+//        System.out.println(selectedoption.getText());
 
-        driver.navigate().to("https://www.spicejet.com/");
+        driver.get("https://www.spicejet.com/");
         driver.findElement(By.xpath("//div[@class=\"r-1862ga2 r-1loqt21 r-1enofrn r-tceitz r-u8s1d css-76zvg2\" and contains(text(),'From')]")).click();
         driver.findElement(By.xpath("//div[contains(text(),'Ahmedabad')]")).click();
+        driver.findElement((By.xpath("//div[contains(text(),\"Pune International Airport\")]"))).click();
+        driver.findElement(By.xpath("//div[text()=\"Departure Date\"]")).click();
+        driver.findElement(By.xpath("//div[contains(text(),\"Family & Friends\")]")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//        WebElement searchFlightBtn = wait.until(ExpectedConditions.elementToBeClickable(
+//                By.xpath("//div[text()='Search Flight']")
+//        ));
+//        searchFlightBtn.click();
+        //driver.findElement(By.xpath("//div[text()=\"Search Flight\"]")).click();
+
+        WebElement searchBtn = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//div[text()='Search Flight']")
+        ));
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", searchBtn);
+        searchBtn.click();
+
 
 
 
